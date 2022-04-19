a941b25aea73d1336bc2370f663a797fc23d48db
Josh L. Minga <joshlminga@gmail.com>
2022-04-19 19:07:50 +0300

    Full dropdownmenu

---
+++ script.js
@@ -0,0 +1,31 @@
+// DropDwon Menu
+const mainMenu = document.querySelector('ultop-nav-menu');
+
+// Get Li item with class has-child
+const hasChild = document.querySelectorAll('li.has-child');
+
+// Loop through each li item with class has-child
+hasChild.forEach(item => {
+    // Get anchortag
+    // const anchor = item.querySelector('a');
+    // Add event listener to each li item
+    item.addEventListener('mouseover', function(e) {
+        // Prevent default behaviour
+        e.preventDefault();
+        // Get the parent li item
+        const dropdwon = this.querySelector('ul.dropdown-menu');
+        // Toggle the child ul item
+        dropdwon.classList.add('active');
+    });
+
+    // Add event lister on Mouse out
+    item.addEventListener('mouseout', function(e) {
+                // Prevent default behaviour
+        e.preventDefault();
+        // Get the parent li item
+        const dropdwon = this.querySelector('ul.dropdown-menu');
+        // Toggle the child ul item
+        dropdwon.classList.remove('active');
+
+    });
+});
